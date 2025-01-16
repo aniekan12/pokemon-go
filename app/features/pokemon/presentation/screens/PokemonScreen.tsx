cc29307799723a3552f78c25189403984b404f1f
fluttergod <akpakpan2535@bazeuniversity.edu.ng>
2025-01-16 11:03:32 +0100

    chore: refactor folder structure

---
+++ app/features/pokemon/presentation/screens/PokemonScreen.tsx
@@ -0,0 +1,10 @@
+import PokemonCard from "@/components/cards/PokemonCard";
+import { View } from "react-native";
+
+export default function PokemonScreen() {
+  return (
+    <View>
+      <PokemonCard />
+    </View>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import PokemonCard from "@/components/cards/PokemonCard";
 import { StatusBar } from "expo-status-bar";
 import { View, Text, SafeAreaView, StyleSheet, Platform } from "react-native";
+import PokemonScreen from "./features/pokemon/presentation/screens/PokemonScreen";
 
 export default function Home() {
   return (
     <View style={style.safeArea}>
-      <PokemonCard />
+      <PokemonScreen />
     </View>
   );
 }
