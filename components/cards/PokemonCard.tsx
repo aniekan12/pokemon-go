a539e7d353be9565a211407ab5d269491fdb6523
fluttergod <akpakpan2535@bazeuniversity.edu.ng>
2025-01-16 10:58:53 +0100

    chore: add style for pokemon card

--- components/cards/PokemonCard.tsx
+++ components/cards/PokemonCard.tsx
@@ -1,12 +1,34 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function PokemonCard() {
   return (
-    <SafeAreaView>
+    <View style={pokemonCardStyle.card}>
       <Text>Pokemon Card</Text>
-    </SafeAreaView>
+    </View>
   );
 }
 
-const pokemonCardStyle = StyleSheet.create({});
+const pokemonCardStyle = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: {
+          width: 2,
+          height: 2,
+        },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+});
