4a79f2fce570d0bc040b74f85ad9b5fe114ddf90
fluttergod <akpakpan2535@bazeuniversity.edu.ng>
2025-01-17 07:04:16 +0100

    chore: flat list styling and section lists

--- app/features/pokemon/presentation/screens/PokemonScreen.tsx
+++ app/features/pokemon/presentation/screens/PokemonScreen.tsx
@@ -6,30 +6,59 @@ import {
   StyleSheet,
   useWindowDimensions,
   FlatList,
+  SectionList,
 } from "react-native";
 import { pokemons } from "../../data/models/Pokemon";
+import pokemonData from "../../data/grouped-data.json";
 
 export default function PokemonScreen() {
   const { width, height } = useWindowDimensions();
   return (
     <View>
       {/* <ScrollView> */}
-      <Text
-        style={[
-          pokemonScreenStyle.title,
-          {
-            fontSize: width > 500 ? 50 : 24,
-          },
-        ]}
+
+      <View
+        style={{
+          paddingHorizontal: 16,
+        }}
       >
-        Pokemons
-      </Text>
-      <FlatList
-        data={pokemons}
-        renderItem={({ item, index }) => (
-          <PokemonCard key={index} props={item} />
-        )}
-      />
+        <SectionList
+          data={pokemonData}
+          sections={pokemonData}
+          renderSectionHeader={({ section: { type } }) => (
+            <Text style={pokemonScreenStyle.title}>{type}</Text>
+          )}
+          SectionSeparatorComponent={() => <View style={{ height: 16 }} />}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => <Text>Item {item}</Text>}
+        />
+        {/* <FlatList
+          keyExtractor={(item, index) => index.toString()}
+          //   horizontal
+          data={pokemons}
+          ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+          renderItem={({ item, index }) => (
+            <PokemonCard key={index} props={item} />
+          )}
+          ListHeaderComponent={
+            <Text style={pokemonScreenStyle.title}>Pokemons</Text>
+          }
+          ListFooterComponent={
+            <Text style={pokemonScreenStyle.title}>Pokemons ended</Text>
+          }
+          ListEmptyComponent={() => (
+            <View
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                flex: 1,
+              }}
+            >
+              <Text>No Items Found</Text>
+            </View>
+          )}
+        /> */}
+      </View>
       {/* {pokemons.map((pokemon) => (
           <PokemonCard props={pokemon} />
         ))} */}
